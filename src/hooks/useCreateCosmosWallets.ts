32e7e6aa14f64c003ab546b8b8a1820ad6f716f3
Lena <elena.mikhaylova0@gmail.com>
2025-05-15 11:17:55 -0400

    add graz

--- src/hooks/useCreateCosmosWallets.ts
+++ src/hooks/useCreateCosmosWallets.ts
@@ -1,7 +1,31 @@
+import { useCallback } from "react";
+import { MinimalWallet } from "@/types/wallet";
+import { WalletType, checkWallet, getWallet } from "graz";
 export const useCreateCosmosWallets = () => {
+  const createCosmosWallets = useCallback(() => {
+    const wallets: MinimalWallet[] = [];
+    const cosmosWallets = getAvailableWallets();
+
+    cosmosWallets.forEach((wallet) => {});
+
+    return wallets;
+  }, []);
+
   return {
-    createCosmosWallets: () => {
-      return [];
-    },
+    createCosmosWallets,
   };
+};
+
+const getAvailableWallets = () => {
+  const browserWallets = [
+    WalletType.KEPLR,
+    WalletType.LEAP,
+    WalletType.COSMOSTATION,
+    WalletType.XDEFI,
+    WalletType.STATION,
+    WalletType.VECTIS,
+    WalletType.WALLETCONNECT,
+  ];
+
+  return browserWallets;
 };
